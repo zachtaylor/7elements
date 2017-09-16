58a92fd8a1586078e73f03030398dab73cb73184
Zach Taylor <zach@ztaylor.me>
2017-09-15 23:47:37 -0500

    redirect hostname will work now?
    it is hard to test things that can only run on production

--- go/src/7elements.ztaylor.me/server/pagehandler.go
+++ go/src/7elements.ztaylor.me/server/pagehandler.go
@@ -12,7 +12,7 @@ var fileserver = http.FileServer(http.Dir(options.String("server-path")))
 var PageHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	if host := options.String("redirect-host"); host != "" && r.RequestURI == "/" && host != r.Host {
 		log.Add("Hostname", host).Add("RequestHost", r.Host).Warn("page: redirected hostname")
-		http.Redirect(w, r, host, 307)
+		http.Redirect(w, r, r.URL.Scheme+"://"+host, 307)
 		return
 	}
 
