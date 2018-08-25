09897cc1e67ac511bf25b52ca12a6685431156ba
Zach Taylor <zach.taylor673@gmail.com>
2018-08-25 13:49:31 -0500

    fix db connection race condition

--- db/conn.go
+++ db/conn.go
@@ -11,15 +11,16 @@ const DB_TABLE = "DB_TABLE"
 
 var conn *db.DB
 
-func init() {
-	tableName := env.Get("DB_TABLE")
-	var err error
-	conn, err = db.Open(tableName)
-	if err != nil {
-		log.Error(err)
-	}
-}
-
 func Patch() (int, error) {
+	if conn == nil {
+		tableName := env.Get(DB_TABLE)
+		var err error
+		conn, err = db.Open(tableName)
+		if err != nil {
+			log.Add(DB_TABLE, tableName).Add(db.DB_HOST, env.Get(db.DB_HOST)).Error(err)
+		} else {
+			log.Add(DB_TABLE, tableName).Add(db.DB_HOST, env.Get(db.DB_HOST)).Debug("loaded db from env")
+		}
+	}
 	return db.Patch(conn)
 }
