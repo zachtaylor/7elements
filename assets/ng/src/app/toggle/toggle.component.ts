11403c7978f5c1ec48042a89e4b6998924d3f80e
Zach Taylor <zach@ztaylor.me>
2019-10-08 11:29:52 -0500

    fixes for --prod

--- assets/ng/src/app/toggle/toggle.component.ts
+++ assets/ng/src/app/toggle/toggle.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
 })
 export class ToggleComponent implements OnInit {
   @Output() check = new EventEmitter()
+  isChecked : boolean
 
   constructor() {
   }
--- assets/ng/src/environments/environment.prod.ts
+++ assets/ng/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true
 };
+window.console.debug = function() {
+};
