a01cb95a88db3eb7c4cf443cf3a4a847f59304ed
Zach Taylor <zach@ztaylor.me>
2019-11-02 12:40:16 -0500

    gofmt

--- game/game.send.go
+++ game/game.send.go
@@ -4,34 +4,34 @@ import "ztaylor.me/cast"
 
 // SendAll sends data to all seats
 func (game *T) SendAll(json cast.JSON) {
-  for _, seat := range game.Seats {
-    seat.Send(json)
-  }
+	for _, seat := range game.Seats {
+		seat.Send(json)
+	}
 }
 
 // SendUpdate creates a data push object, and uses SendAll
 func (game *T) SendUpdate(uri string, data cast.JSON) {
-  game.SendAll(BuildPushJSON(uri, data))
+	game.SendAll(BuildPushJSON(uri, data))
 }
 
 // SendStateUpdate sends game state data using SendUpdate
 func (game *T) SendStateUpdate() {
-  game.SendUpdate("/game/state", game.State.JSON())
+	game.SendUpdate("/game/state", game.State.JSON())
 }
 
 // SendSeatUpdate sends game seat data using SendUpdate
 func (game *T) SendSeatUpdate(seat *Seat) {
-  game.SendUpdate("/game/seat", seat.JSON())
+	game.SendUpdate("/game/seat", seat.JSON())
 }
 
 // SendPrivateUpdate sends game private data only to a seat
 func (game *T) SendPrivateUpdate(seat *Seat) {
-  seat.Send(BuildPushJSON("/game", game.PerspectiveJSON(seat)))
+	seat.Send(BuildPushJSON("/game", game.PerspectiveJSON(seat)))
 }
 
 // SendCardUpdate sends card data using SendDataUpdate
 func (game *T) SendCardUpdate(c *Card) {
-  game.SendUpdate("/game/card", c.JSON())
+	game.SendUpdate("/game/card", c.JSON())
 }
 
 // SendReactUpdate sends JSON command to update state reaction
