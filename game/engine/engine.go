14e8620408b075f95ee06501b48549e8b5de29b5
Zach Taylor <zach@ztaylor.me>
2020-04-18 08:59:01 -0500

    engine update
    addresses cpu usage, reduce wakinig game thread

--- game/engine/engine.go
+++ game/engine/engine.go
@@ -1,8 +1,6 @@
 package engine
 
 import (
-	"time"
-
 	"github.com/zachtaylor/7elements/game"
 	"github.com/zachtaylor/7elements/game/update"
 	"ztaylor.me/cast"
@@ -10,72 +8,62 @@ import (
 
 // Run the engine
 func Run(g *game.T) {
-	tStart := time.Now()
-	one2nd := time.NewTicker(time.Second)
-	var tStep time.Time
+	var tStart cast.Time
 
 	enginelog := g.Runtime.Root.Logger.New().With(cast.JSON{
 		"GameID": g.ID(),
 	}).Tag("engine")
 	enginelog.Info("start")
 
-	activate(g) // bootstrap
+	// bootstrap
+	activate(g)
+	timer := cast.NewTimer(g.State.Timer)
+
 loop: // nested break
 	for { // event loop
-		tStep = time.Now()
-		select { // read request chan OR tick timer
-		case r, ok := <-g.Monitor():
+		tStart = cast.Now()
+		select { // read request chan or timeout
+		case <-timer.C: // timeout
+			g.Log().Tag("engine/timeout")
+			resolve(g)
+		case r, ok := <-g.Monitor(): // player noise
+			if !timer.Stop() {
+				<-timer.C
+			}
+			g.State.Timer -= cast.Now().Sub(tStart)
+
 			if !ok {
-				g.Log().Info("closed")
+				g.Log().Source().Info("stopping")
 				break loop // nested break
 			}
-			g.Log().With(cast.JSON{
+
+			log := g.Log().With(cast.JSON{
 				"Path":     r.URI,
 				"Username": r.Username,
-			}).Debug("engine: request")
-			stack(g, request(g, g.GetSeat(r.Username), r.URI, r.Data))
-			continue
-		case <-one2nd.C:
-		} // time passed, requests were processed
-		g.State.Timer -= time.Now().Sub(tStep)
+				"State":    g.State.Name(),
+			}).Tag("engine/request")
 
-		log := g.Log().Add("State", g.State.Print()).Tag("engine/tick")
+			if states := request(g, g.GetSeat(r.Username), r.URI, r.Data); len(states) < 1 {
+				log.Debug("no stack")
+			} else {
+				log.Copy().Add("Stack", states).Debug("stacking")
+				stack(g, states)
+			}
 
-		if len(g.State.Reacts) == len(g.Seats) {
-			log.Debug("resolve")
-		} else if g.State.Timer < time.Second {
-			log.Warn("timeout")
-		} else {
-			continue
-		} // move on
-
-		// change state
-		g.State.Timer = 0
-		events := finish(g)                       // save event stack
-		if state := g.State.Stack; state != nil { // stack pop
-			log.Add("NextState", state).Debug("stackpop")
-			g.State = state
-			g.State.Timer = g.Runtime.Timeout
-			g.State.Reacts = make(map[string]string)
-			update.State(g)
-			connect(g, nil)
-		} else if e := g.State.R.GetNext(g); e != nil { // next state
-			log.Add("Next", e).Debug("getnext")
-			g.State = g.NewState(e)
-			events = append(events, activate(g)...)
-		} else {
-			log.Info("exit")
-			break loop // nested break
+			if len(g.State.Reacts) == len(g.Seats) {
+				log.Info("resolve")
+				resolve(g)
+			} else if g.State.Timer < cast.Second {
+				log.Warn("timeout")
+				resolve(g)
+			} else {
+				log.Debug()
+			}
 		}
-
-		// stack events
-		if len(events) > 0 {
-			g.Log().Add("Events", events).Debug("stacking")
-			stack(g, events)
-		}
+		timer.Reset(g.State.Timer)
 	} // event loop
 
-	enginelog.Add("Data", g.State.R.JSON()).Add("Time", time.Now().Sub(tStart)).Info("end")
+	enginelog.Add("Data", g.State.R.JSON()).Add("Time", cast.Now().Sub(tStart)).Info("end")
 	for _, seat := range g.Seats {
 		if seat.Receiver != nil {
 			seat.Receiver.WriteJSON(update.Build("/game", nil))
